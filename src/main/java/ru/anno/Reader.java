11d17166968eba9e2a5fa255e355d934f565f44a
avfyodorov <avfyodorov60@yandex.ru>
2021-08-20 19:31:07 +0300

    Lesson 13

--- src/main/java/ru/anno/Reader.java
+++ src/main/java/ru/anno/Reader.java
@@ -6,12 +6,15 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Reader {
+//   @Autowired
+//   @Qualifier("book")
    private Paper paper;
 
    @Autowired
    public Reader(@Qualifier("book")Paper paper) {
       this.paper = paper;
    }
+//   public Reader(Paper paper) { this.paper = paper;  }
 
    public void whatRead() {
       System.out.println("Now reading: " + paper.getName());
